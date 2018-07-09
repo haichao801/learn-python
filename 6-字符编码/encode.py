5fe2324269ee48a6ef9d709feed58f183e2e68b8
haichao801 <349133376@qq.com>
2018-07-10 07:50:11 +0800

    encode.py

---
+++ 6-字符编码/encode.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+s = "路飞"
+print(s)
+
+s2 = s.encode("gbk")
+print('s2=', s2)  # gbk
+print('type(s2):', type(s2))
+
+
+# s4 = s2.decode("utf-8")
+# print('s4=', s4)
+# print('type(s4):', type(s4))  # unicode
+
